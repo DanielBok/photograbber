aee74b498a49b99574ba3cbfd65c4ca048134e7e
Daniel Bok <daniel.bok@outlook.com>
2024-02-22 17:46:10 +0800

    pyenv and pipfiles

--- main.py
+++ main.py
@@ -5,17 +5,6 @@ import click
 from PhotoGrab import PhotoGrab
 
 
-@click.command()
-def cli():
-    """Copies the photos"""
-    t = time.time()
-    PhotoGrab().run()
-    total_time = round(time.time() - t, 5)
-    click.echo(f"Complete. Elapsed time: {total_time}s")
-
-
 if __name__ == "__main__":
-    click.echo("If you're seeing this message, you're using the program incorrectly")
-    click.echo("You should instead activate the photo environment first")
-    click.echo("Subsequently, just type 'photo'")
+    PhotoGrab().run()
 
